25cc115f3eee2c619f6419188553311502f560f5
hrttn <hrettien@gmail.com>
2018-03-19 16:01:22 +0100

    first commit

---
+++ please-come-back-title.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Please Come Back Title
+ * Description: Bring your users back by changing the title of your page when they leave the tab.
+ * Version: 1.0.0
+ * Author: WP Elk
+ *
+ * @package    WPElk
+ * @subpackage PleaseComeBack
+ * @author     WP Elk <hugo@hugorettien.com>
+ * @link       https://wpelk.com
+ */
+
+namespace WPElk\PleaseComeBack;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
